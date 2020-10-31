cbde29e860d93fe2d5847b6f638933e3a2ce008f
kristian andersen <krilleandersen@gmail.com>
2020-10-31 22:27:31 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,135 +1,157 @@
-/*
-todo :
-✔️ load json
-✔️ current session
-✔️ what is current user watching, have watched and purchased
-✔️ mosthasviewed
-✔️ match what current user is viewing with what current user has viewed most 
-
-functions for
-    highest rated recomandations
-    matches most viewed not purchased recomandations
-    not matches most viewed not purchased recomandations
-
-greeting template with:    
-    name
-    highest rated recomandations    
-    matches most viewed not purchased recomandations
-    or
-    not matches most viewed not purchased recomandations
-*/ 
+const products = require("./assets/json/Products.json");
+const users = require("./assets/json/Users.json");
+const currentUserSession = require("./assets/json/CurrentUserSession.json");
 
 
-/*Because it is easy, and it is not something for production
-i'll just require the json files */
+//welocm greeting
+function greetings() {
+  obr = overallbedstrating();
 
-let products = require("./assets/json/Products.json");
-let users = require("./assets/json/Users.json");
-let cus = require("./assets/json/CurrentUserSession.json");
+  let movie1 = obr[0].name;
+  let movie2 = obr[1].name;
+  let movie3 = obr[2].name;
 
+  let rate1 = obr[0].rating;
+  let rate2 = obr[1].rating;
+  let rate3 = obr[2].rating;
 
-/*get the current session */
-function getcursession(){
-    
-   cus.forEach((obj) => {
-        
-        let uid=obj.userid
-        let viewing=obj.productid
+let topgreeting = `-------------------------------------------------
+Our Top 3 Rated Movies this week is\n
+${movie1} with a super rating of ${rate1}
+${movie2} with a splendid rating of ${rate2}
+${movie3} with a nice rating of ${rate3}
+-------------------------------------------------`;
+  console.log(topgreeting);
 
-        const curentusers = users.filter((cu) => cu.id === uid);
+  currentUserSession.forEach((cuser) => {
+    let theuser = getuser(cuser.userid);
+
+    let name = theuser.name;
+    let recommended = theuser.recommended;
+    let keys = theuser.keywords;
+
+    function recommovies(){
+        let rrname=[];
+  
+        recommended.forEach((r) => {
+        rrname.push(r.name+' Rated:'+r.rating+'\n');
+   
+        });
+        rrname=rrname.join(' ')
        
-        let id=curentusers[0].id;
-        let name=curentusers[0].name;
-        let purchased=curentusers[0].purchased;
-        let viewed=curentusers[0].viewed; 
-        
-            purchased=purchased.split(";")
-            viewed=viewed.split(";")
+        return rrname;
 
-        curentuserslist(id,name,purchased,viewed,viewing)
-
-    })
-
-
-}
-getcursession()
-
-
-
-/*what is our current user watching, have watched and purchased */
-function curentuserslist(id,name,purchased,viewed,viewing){
- 
-   /*create unique array from purchased & viewed of only viewed not purchased*/
-    let uniqueArr = []; 
-    let viewedNotPurchased=unique(purchased,viewed,uniqueArr) 
-
-    let hasviewed =[];
-    let hasviewedkeys =[];
-    let hasviewedname =[];
-
-    let isviewing=[]
-    let isviewingkey=[]
-    let isviewingname=[]
-
-    /*what has been */
-    for (i = 0; i < viewedNotPurchased.length; i++) { 
-    
-        hasviewed = products.filter((hv) => hv.id == viewedNotPurchased[i]);      
-        hasviewedkeys.push(...hasviewed[0].keywords)
-        hasviewedname.push(hasviewed[0].name)
-      
     }
-    //clean uot array of empty slots looks neat
-    hasviewedkeys=hasviewedkeys.filter(n => n)
+let greeting = `=================================================
+Hi there ${name}
+We see you like '${keys.join(' ')}\n
+So we have these recommendations for you \n${ recommovies()}
+=================================================`;
 
-    //create  array from what current user is viewing*/
-    isviewing = products.filter((iv) => iv.id == viewing); 
-    isviewingkey.push(...isviewing[0].keywords)
-    isviewingname.push(isviewing[0].name)
-
-    /*Find the most viewed/purchased keyword for each user */
-    mosthasviewed=hasviewedkeys.sort((a,b) => hasviewedkeys.filter(v => v===a).length - hasviewedkeys.filter(v => v===b).length).pop();
-    
-
-    for (i = 0; i < isviewingkey.length; i++) {    
-      
-        /* if what current user is viewing matches most viewed not purchased */
-        if(isviewingkey[i]===mosthasviewed){
-                console.log(name+'\n'+isviewingkey[i]+'--'+mosthasviewed)
-        }
-
-        /* if what current user is viewing do not matches most viewed not purchased */
-        if(isviewingkey[i]!==mosthasviewed){
-            console.log(name+'\n'+mosthasviewed)
-            return
-        }
-    }
-
+    console.log(greeting);
+  });
 }
 
+greetings();
 
+/*get  user by id and get the info we need */
+function getuser(id) {
+  const user = users.find((object) => object.id === id);
+  const curuser = currentUserSession.find((object) => object.userid === id);
+
+  //let viewedAry = makearray(user.viewed);
+  let purchasedAry = makearray(user.purchased);
+  let keywords = getkeywords(purchasedAry);
+  //let viewingkey = currentviewing(curuser.productid);
+  let recommended = notpurchased(purchasedAry, keywords);
+
+  let userinfo = {
+    id: user.id,
+    name: user.name,
+    recommended: recommended,
+    keywords: keywords,
+  };
+
+  return userinfo;
+}
+
+function overallbedstrating() {
+  let bedstrating;
+
+  bedstrating = products.sort((a, b) => Number(b.rating) - Number(a.rating));
+  bedstrating = bedstrating.filter((notpurchasedmovies, i) => i < 3);
+
+  return bedstrating;
+}
+
+function getkeywords(idforkeys) {
+  let haspurchased = [];
+  let haspurchasedkeys = [];
+
+  idforkeys.forEach((key) => {
+    haspurchased = products.filter((hv) => hv.id == key);
+    haspurchasedkeys.push(...haspurchased[0].keywords);
+  });
+
+  haspurchasedkeys = haspurchasedkeys.filter((n) => n);
+  haspurchasedkeys=haspurchasedkeys.sort()
+
+  return getMostCommon(haspurchasedkeys);
+}
+
+//filetr out what user has purchased
+function notpurchased(pary, vkey) {
+  let notpurchasedmovies = [];
+  products.forEach(function (a) {
+    //get all movies that is not alredy in the purchasedAry
+    if (!pary.includes(a.id)) {
+      //get only the movies that include users top keywords
+      if (vkey.some((i) => a.keywords.includes(i))) {
+        notpurchasedmovies.push(a);
+      }
+    }
+  });
+  //sort on ratings
+  notpurchasedmovies.sort((a, b) => Number(b.rating) - Number(a.rating));
+
+  //get the three highest rated movies
+  notpurchasedmovies = notpurchasedmovies.filter(
+    (notpurchasedmovies, i) => i < 3
+  );
+
+  return notpurchasedmovies;
+}
+
+//current user product id
+function currentviewing(id) {
+  let curkey = products.filter((hv) => hv.id == id);
+  let keys = curkey[0].keywords;
+
+  return keys;
+}
+
+/*helper function to convert a string to array of numbers*/
+function makearray(str) {
+  let strnumbers = str.split(";").map(Number);
+  return strnumbers;
+}
 
 /*
-helper function to create a unique arry from 
-purchased and viewed to prevent recomadations on 
-already purchased movies.
-*/ 
-function unique(arr1,arr2,uniqueArr) { 
-    for(var i=0; i<arr1.length; i++) { 
-        flag = 0; 
-        for(var j=0; j<arr2.length; j++) { 
-            if(arr1[i] === arr2[j]) { 
-                arr2.splice(j,1); 
-                j--; 
-                flag = 1; 
-            } 
-        } 
-  
-        if(flag == 0) { 
-            uniqueArr.push(arr1[i]); 
-        } 
-    } 
-    uniqueArr.push(arr2); 
-  
-    return uniqueArr.flat(); 
+helper function to get most common keywords
+of generes the user has viewed but not purchased
+*/
+function getMostCommon(array) {
+  var count = {};
+  array.forEach(function (a) {
+    count[a] = (count[a] || 0) + 1;
+  });
+  return Object.keys(count).reduce(function (r, k, i) {
+    if (!i || count[k] > count[r[0]]) {
+      return [k];
+    }
+    if (count[k] === count[r[0]]) {
+      r.push(k);
+    }
+    return r;
+  }, []);
 }
